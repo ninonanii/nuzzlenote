a54776c68e54da0b580e0d0d2619f8f61f6e00ca
dom <17290358+zukinii@users.noreply.github.com>
2024-04-14 00:14:16 +0200

    pwa manifest and service worker

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to Nuzzlenote</h1>
+<p>offline-first, customizable cozy writing space</p>
+
+<style>
+	h1 {
+		margin: var(--size-2);
+
+		@media (--md-n-above) {
+			margin: var(--size-5);
+		}
+	}
+</style>
